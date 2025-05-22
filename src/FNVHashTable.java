f10735fd796a31f8166830defa5a5d240e7b140c
kyoulau <laucosan05@gmail.com>
2025-05-22 20:04:26 -0300

    Initial commit

---
+++ src/FNVHashTable.java
@@ -0,0 +1,16 @@
+public class SecondFunctionHash extends HashTable{
+    @Override
+    boolean insert(String name) {
+        return false;
+    }
+
+    @Override
+    boolean delete(String name) {
+        return false;
+    }
+
+    @Override
+    boolean search(String name) {
+        return false;
+    }
+}
---
+++ src/SeparateChainingHashTable.java
@@ -0,0 +1,16 @@
+public class FunctionHash extends HashTable{
+    @Override
+    boolean insert(String name) {
+        return false;
+    }
+
+    @Override
+    boolean delete(String name) {
+        return false;
+    }
+
+    @Override
+    boolean search(String name) {
+        return false;
+    }
+}
